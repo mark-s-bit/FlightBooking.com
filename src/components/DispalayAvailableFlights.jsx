fb544f40fd8001d0eab11cb123fd9be4b2d21a45
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-08-15 16:31:42 +0300

    fetched flights

--- src/components/DispalayAvailableFlights.jsx
+++ src/components/DispalayAvailableFlights.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import SearchFlight from "./SearchFlight";
 import FlightDetails from "./FlightDetails";
-import "./DisplayAvailableFlights.css" 
+import "./DisplayAvailableFlights.css";
+import { useState, useEffect } from "react";
 
 function DispalayAvailableFlights() {
+  const url = "http://localhost:3000/flights/";
+  const [flights, setFlights] = useState([]);
+
+  useEffect(() => {
+    const fetchFlights = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setFlights(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchFlights();
+  }, []);
+
+  console.log(flights);
+
   return (
     <div className="Container">
       <SearchFlight />
       <div className="FlightsWrapper">
-        <FlightDetails />
+        <FlightDetails flights={flights} />
       </div>
     </div>
   );
